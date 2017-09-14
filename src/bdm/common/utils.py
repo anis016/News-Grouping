6bc2f9e483cc4d20ba2517d86bf644512d503060
hoque <sayed.hoque@st.ovgu.de>
2017-09-14 16:33:50 +0200

    data finder added

--- src/bdm/common/utils.py
+++ src/bdm/common/utils.py
@@ -10,10 +10,20 @@ def convertTimeStampToDataTime(timeStamp):
 
 def parse_date(article):
     meta_data = article.meta_data
-    for
+    # parse dict
     pass
     #  = article.meta_description
     # datetime = DateService.extractDates(meta_description)
+
+data_set = '''
+'Washington DC (SPX) Jun  - 
+With 14 electric motors turning propellers and all of them integrated into a uniquely-designed wing, NASA will test new propulsion technology using an experimental airplane now designated the X-57 a'
+'''
+
+def date_finder(st):
+    matches = datefinder.find_dates(st)
+    for match in matches:
+        print(match)
 
 def normalize_date(line):
     pass
