c7802f881eb2dcdd06b9eb7c5baad657ef067729
Sayed Anisul Hoque <sayed.hoque@st.ovgu.de>
2017-11-13 01:37:40 +0100

    Added a check if an internet connection is present while downloading.

--- src/bdm/common/utils.py
+++ src/bdm/common/utils.py
@@ -1,4 +1,6 @@
 import datetime
+import socket
+
 from dateutil.parser import parse
 import re
 import urllib3
@@ -132,5 +134,17 @@ def get_mongo_value(keyword, document):
 
     return mongo_value
 
+
+def is_connected():
+    try:
+        # connect to the host -- tells us if the host is actually
+        # reachable
+        socket.create_connection(("www.google.com", 80))
+        return True
+    except OSError:
+        pass
+    return False
+
 if __name__ == '__main__':
-    print(date_finder(CONSTANTS.data_set4))
+    # print(date_finder(CONSTANTS.data_set4))
+    print(is_connected())
