e69e15aa8809a26bf7d8035d76523806fc237e3d
muashraf <muhammad.ashraf@ovgu.de>
2017-11-30 19:52:35 +0100

    added new routes

--- src/bdm/www/bdm/views.py
+++ src/bdm/www/bdm/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.shortcuts import render_to_response
+from django.shortcuts import render, get_object_or_404
 from django.template import RequestContext
 from mongo.MongoDB import MongoDB
 import common.constants as CONSTANTS
@@ -9,4 +9,13 @@ def index(request):
     mongoOb = MongoDB()
     mongoOb.initialzie()
     news = mongoOb.find(CONSTANTS.COLLECTION_SUBSETNEWS)
-    return render_to_response('index.html', {'News': news}, RequestContext(request))
+    return render(request, "index.html", {'News': news})
+
+def detail(request, id=None):
+    return HttpResponse("hahaha")
+
+def about(request):
+    return render(request, "about.html", {})
+
+def contact(request):
+    return render(request, "contact.html", {})
