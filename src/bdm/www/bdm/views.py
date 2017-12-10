4783136c6547a08f449a27688f184061d64b899e
muashraf <muhammad.ashraf@ovgu.de>
2017-12-10 21:03:20 +0100

    Django: Detail page view, index query search and Pie chart integration

--- src/bdm/www/bdm/views.py
+++ src/bdm/www/bdm/views.py
@@ -16,14 +16,17 @@ def index(request):
     mongoOb = MongoDB()
     mongoOb.initialzie()
     if request.method == 'POST' and request.POST.get('txtSearch', '') != "" and request.POST.get('page', '') != "":
-        total = mongoOb.find("samplenewsSubsetCollection", {"cleaned_title": {"$regex": u".*" + request.POST.get('txtSearch', '').strip() + ".*"}}).count()
+        txtSearch = request.POST.get('txtSearch', '').strip()
+        total = mongoOb.find("samplenewsSubsetCollection", {"$or": [{"cleaned_title": {"$regex": u".*" + txtSearch + ".*"}}, {"cleaned_body": {"$regex": u".*" + txtSearch + ".*"}}]}
+).count()
         limit = 2
         total_pages = math.ceil(total / limit)
         offset = (int(request.POST.get('page', '')) - 1) * 2;
         nxtPage = int(request.POST.get('page', '')) + 1 if int(request.POST.get('page', '')) < total_pages else ''
-        news = mongoOb.find("samplenewsSubsetCollection", {"cleaned_title": {"$regex": u".*" + request.POST.get('txtSearch', '').strip() + ".*"}}).skip(offset).limit(limit)
+        news = mongoOb.find("samplenewsSubsetCollection", {"$or": [{"cleaned_title": {"$regex": u".*" + txtSearch + ".*"}}, {"cleaned_body": {"$regex": u".*" + txtSearch + ".*"}}]}
+).skip(offset).limit(limit)
         if(news.count() != 0):
-            return render(request, "index.html", {'news': news, 'txtSearch': request.POST.get('txtSearch', '').strip(), 'nxtPage': nxtPage,  'prevPage': int(request.POST.get('page', '')) - 1})
+            return render(request, "index.html", {'news': news, 'txtSearch': txtSearch, 'nxtPage': nxtPage,  'prevPage': int(request.POST.get('page', '')) - 1})
         else:
             return render(request, "index.html", {})
     else:
@@ -59,20 +62,24 @@ def detail(request):
     mongoOb = MongoDB()
     mongoOb.initialzie()
     if request.method == 'GET' and request.GET.get('obj', '') != "":
+        totalNews = mongoOb.find("samplenewsSubsetCollection").count()
         news = mongoOb.find("samplenewsSubsetCollection", {"_id": ObjectId(request.GET.get('obj', '').strip())})
         for record in news:
             cleaned_title = record['cleaned_title']
             url = record['url']
             connected = record['connected']
 
-        if(connected != "None"):
-            objIds = [x.strip() for x in connected.split(',')]
-            documents = []
-            for docId in objIds:
+        connected.remove("None") if connected[0]=="None" else ''
+        unlikeNews = totalNews - len(connected)
+        unlikeNewsProp = float(unlikeNews) * (100.0 / totalNews)
+        similarNewsProp = float(len(connected)) * (100.0 / totalNews)
+        documents = []
+        for docId in connected:
+            if(docId != ""):
                 documents.append(getSimilarNews(docId))
-            return render(request, "detail.html", {'cleaned_title': cleaned_title, 'url': url, 'news_stats':3, 'similarDocs': documents})
-        else:
-            return render(request, "detail.html", {'cleaned_title': cleaned_title, 'url': url, 'news_stats':3})
+        return render(request, "detail.html", {'cleaned_title': cleaned_title, 'url': url, 'similarNews': similarNewsProp, 'unlikeNews': unlikeNewsProp, 'similarDocs': documents})
+    else:
+        return render(request, "detail.html", {})
 
 def getSimilarNews(docId):
     mongoOb = MongoDB()
